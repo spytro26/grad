44ca42527f54c8fd2758835edde575b857059fbf
spytro26 <2654anku@gmail.com>
2026-05-30 13:57:02 +0530

    morning assignment

--- day12/back/index.js
+++ day12/back/index.js
@@ -16,24 +16,25 @@ app.use(express.json());
 // }
 
 function getToken(){
-    let arr = [ 1 , "a" , "b" , "s" , 2 , 3 , 4];
-    let  ab="";
-    let size = arr.length -1 ;  
-    for(let i = 0; i<10; i++){
-    ab +=   arr[parseInt(size * Math.random())]
+    let arr = [ 1 , "a" , "b" , "s" , 2 , 3 , 4 , 6, 7, 8, 9 , "c" , "e"];
+    let  token="";
+    let size = arr.length -1     ;  
+    for(let i = 0; i<20; i++){
+    token = token +   arr[parseInt(size * Math.random())]
     }
     
-    return "123"; 
+    return token; 
 }
 
-function  ispresent(username  ){
+function  ispresent(x  ){
 
     for(let i =0; i<users.length ; i++){
-                if(users[i].username == username){
+                if(users[i].username == x){
                     return false;
                 }
 
     }
+
     return true; 
 
 }
@@ -47,6 +48,10 @@ app.get("/" , (req , res )=>{
 
 app.post("/signup" , (req , res)=>{
     const {name , username , password } = req.body;
+    if(!name || !username  || !password){
+        return res.json({message : "missing data"});
+
+    }
 
 
       // username ? 
@@ -57,7 +62,7 @@ app.post("/signup" , (req , res)=>{
         return res.status(400).json({message : "weak password"})
       }
 
-      if(!ispresent(username)){
+      if(ispresent(username)==false ){
         return res.status(401).json({message : "username already taken "});
 
       }
@@ -65,7 +70,7 @@ app.post("/signup" , (req , res)=>{
       a++
       users.push({
         id : a  , 
-        name   ,
+        name    ,
         username : username  , 
         password  : password  ,
         balance : 2000  
@@ -75,7 +80,7 @@ app.post("/signup" , (req , res)=>{
       })
       
 
-      return res.status(201).json({msg : "user id created succefully" , a });
+      return res.status(201).json({msg : "user id created succefully" , a   });
 
 
 
@@ -85,6 +90,7 @@ app.post("/signup" , (req , res)=>{
 
 app.post("/signin" , (req , res)=>{
     const{username , password} = req.body;
+
     let  userPresent = 0;
     let userId  ; 
     for(let i =0; i<users.length ; i++){
@@ -121,26 +127,26 @@ app.post("/signin" , (req , res)=>{
 
 function checkAuth(req , res , next){
     const {id , token} = req.body;
-    console.log("middleware");
+ 
     for(let i =0; i<loginedUsers.length; i++)
     {
         if(loginedUsers[i].id == id && loginedUsers[i].token == token){
             // req.userid = id; 
-            next();
+         return    next();
         }
     }
 
-    console.log("not next");
+   
     return res.json({message : "invalid user"});
 
 }
 
 
-app.use(checkAuth);
-
-app.get("/bal" , (req , res)=>{
+ app.use(checkAuth);
+// improvement add upi pin functionality 
+app.get("/bal"  ,  (req , res)=>{
     // id password 
-    console.log("hiteed");
+ 
     const {id} = req.body;
     for(let i =0; i<users.length ; i++){
         if(id == users[i].id){
@@ -156,13 +162,85 @@ app.get("/bal" , (req , res)=>{
     
 })
 
+// money transer user 1 - > 2 
+
+// same improvment as /bal endpoint
+ function moneytransfer(req , res){
+    const {id , recId , balance} = req.body;
+    if(!recId || !balance){
+        return ; 
+
+    }
+       let sender ; 
+       for(let i=0; i<users.length ; i++){
+            if(users[i].id == id){
+                sender = users[i];
+            }
+       }  
+
+       if(balance > sender.balance){
+        return  res.json({message : "insufficent money"});
+       }
+
+       for(let i =0; i<users.length ; i++){
+           if(users[i].id == id ){
+               users[i].balance = users[i].balance - balance;
+           }
+           if(users[i].id == recId){
+            users[i].balance = users[i].balance + balance;
+           }
+       } 
+
+       return res.json({message : "money transfered"});
+
+
+    
+ }
+
+
+
+
+app.post("/transfer"  , moneytransfer);
+
+
+
+
+
+
+            // assignment 
+
+
+
+
+
+ // returns the user data like its name , username , etc
 app.get("/userDetails"  , (req , res)=>{
 
 })
 
-app.post("/kyc" , (req , res)=>{
+
+// update the user Data like changing the username , name phone etc , 
+
+app.post("/userDetails" , (req , res)=>{
 
 })
+// update the usesr balance with the credited money 
+app.post("/creditmoney" , (req ,res)=>{
+
+})
+
+// atm withdrwaal 
+app.post("/debitmoney" ,(req, res)=>{
+
+})
+
+// it should return something like bank statement 
+// should must conatain - sender reciver balance senderId , reciverId ; 
+
+app.post("/showhistory", (req, res)=>{
+
+})
+
 
 
 app.listen(3000);
